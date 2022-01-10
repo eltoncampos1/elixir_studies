b31891c3a099e39a42e99327b79d77745b85d867
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-10 17:33:16 -0300

    chore: add credo and bcrypt

--- rocketpay/mix.exs
+++ rocketpay/mix.exs
@@ -44,7 +44,9 @@ defmodule Rocketpay.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.5" , only: [:dev, :test], runtime: false},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
