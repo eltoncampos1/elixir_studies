167e0515c171b0436ada308bac860c1dedca4d17
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-11 12:55:26 -0300

    chore: add decimal lib

--- rocketpay/mix.exs
+++ rocketpay/mix.exs
@@ -46,7 +46,8 @@ defmodule Rocketpay.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5" , only: [:dev, :test], runtime: false},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:bcrypt_elixir, "~> 2.0"},
+      {:decimal, "~> 2.0"}
     ]
   end
 
