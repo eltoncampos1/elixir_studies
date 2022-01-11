cee98c4b8222e39451a9d040e579c4773b8abdc7
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-10 21:41:00 -0300

    feat: create user with account

--- rocketpay/lib/rocketpay/users/create.ex
+++ rocketpay/lib/rocketpay/users/create.ex
@@ -1,9 +1,35 @@
 defmodule Rocketpay.Users.Create do
-  alias Rocketpay.{Repo, User}
+  alias Rocketpay.{Account, Repo, User}
+  alias Ecto.Multi
 
   def call(params) do
-    params
-      |> User.changeset
-      |> Repo.insert
+    Multi.new()
+      |> Multi.insert(:create_user, User.changeset(params))
+      |> Multi.run(:create_account, fn repo, %{create_user: user} -> insert_account(repo, user.id)
+      end)
+      |>  Multi.run(:preload_data, fn repo, %{create_user: user} ->
+        preload_data(repo, user)
+      end)
+      |> run_transaction()
+  end
+
+  defp insert_account(repo, user_id) do
+    user_id
+      |> account_changeset()
+      |> repo.insert()
+  end
+
+  defp preload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp account_changeset(user_id), do: Account.changeset(%{user_id: user_id, balance: "0.00"})
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
+
   end
 end
