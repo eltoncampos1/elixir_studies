12c1e31c0ce9571ab01765b8600e92d8e245fdb8
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-13 19:53:43 -0300

    refactor: validate if number already exists

--- telephony/lib/subscriber.ex
+++ telephony/lib/subscriber.ex
@@ -1,21 +1,37 @@
 defmodule Subscriber do
-  defstruct name: nil, number: nil, cpf: nil
+  defstruct name: nil, number: nil, cpf: nil, plan: nil
 
-  @filename "subscribers.txt"
+  @subscribers%{
+    :prepaid => "plans/pre.txt",
+    :postpaid => "plans/post.txt"
+  }
 
-    def register(name, number, cpf) do
-      read() ++ [%__MODULE__{name: name, number: number, cpf: cpf}]
+  def register(name, number, cpf, plan) do
+    case find_subscriber_by_number(number) do
+      nil ->
+      read(plan) ++ [%__MODULE__{name: name, number: number, cpf: cpf, plan: plan}]
         |> :erlang.term_to_binary()
-        |> write()
+        |> write(plan)
+
+        {:ok, "Subscriber #{name} has been successfully registered"}
+        _subscriber ->
+          {:error, "Subscriber with this number already registered"}
+    end
     end
 
-    defp write(subscribers_list) do
-      File.write!(@filename, subscribers_list)
-    end
 
-    defp read() do
-      {:ok, subscribers } = File.read(@filename)
-      subscribers
-        |> :erlang.binary_to_term
-    end
+  defp write(subscribers_list, plan) do
+    File.write!(@subscribers[plan], subscribers_list)
+  end
+
+  def read(plan) do
+    {:ok, subscribers } = File.read(@subscribers[plan])
+    subscribers
+      |> :erlang.binary_to_term
+  end
+
+  def find_subscriber_by_number(number) do
+    read(:prepaid) ++ read(:postpaid)
+      |> Enum.find(fn sub -> sub.number == number end)
+  end
 end
