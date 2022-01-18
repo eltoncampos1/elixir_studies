1c3a5e062fc443875371624a1587d8f293f3f1df
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-18 15:49:02 -0300

    refactor: transfer and register

--- bank/lib/account.ex
+++ bank/lib/account.ex
@@ -1,22 +1,38 @@
 defmodule Account do
   defstruct user: User, balance: 1000
 
-  def register(user), do: %__MODULE__{user: user}
+  @accounts "accounts.txt"
 
-  def transfer(accounts, from, to, value) do
-    from = Enum.find(accounts, fn acc -> acc.user.email == from.user.email end)
+  def register(user) do
+    case find_by_email(user.email) do
+      nil ->
+        binary = [%__MODULE__{user: user}] ++ find_accounts()
+        |> :erlang.term_to_binary()
+        File.write(@accounts, binary)
+      _ -> {:error, "User already registered."}
+    end
+
+  end
+
+  def transfer(from, to, value) do
+    from = find_by_email(from)
+    to = find_by_email(to)
 
     cond do
       validate_balance(from.balance, value) -> {:error, "Insufficient funds"}
       true ->
-        to = Enum.find(accounts, fn acc -> acc.user.email == to.user.email end)
+        accounts = delete([from, to])
         from = %Account{from | balance: from.balance - value}
         to = %Account{to | balance: to.balance + value}
-        [from, to]
+        accounts = accounts ++ [from, to]
+        Transaction.record("transfer", from.user.email, value, Date.utc_today(), to.user.email)
+        File.write(@accounts, :erlang.term_to_binary(accounts))
+
     end
   end
 
   def withdraw(account, value) do
+    account = find_by_email(account)
     cond do
       validate_balance(account.balance, value) -> {:error, "Insufficient funds"}
       true ->
@@ -25,6 +41,17 @@ defmodule Account do
     end
   end
 
+  def find_accounts do
+    {:ok, binary} = File.read(@accounts)
+    :erlang.binary_to_term(binary)
+  end
+
+  def delete(delete_accounts) do
+    Enum.reduce(delete_accounts, find_accounts(), fn c, acc -> List.delete(acc, c)end)
+  end
+
   defp validate_balance(balance, value), do: balance < value
+
+  defp find_by_email(email), do:  Enum.find(find_accounts(), &(&1.user.email == email))
 
 end
