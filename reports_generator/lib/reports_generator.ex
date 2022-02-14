d8606c3058a026e04198872b87451d8b13e38659
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-14 16:54:34 -0300

    new project reports generator

---
+++ reports_generator/lib/reports_generator.ex
@@ -0,0 +1,16 @@
+defmodule ReportsGenerator do
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.map(fn line ->
+      parse_line(line)
+    end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
