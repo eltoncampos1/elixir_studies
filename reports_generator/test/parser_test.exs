f15355ca4a5cd368bd4bb970bc99307cf46f921d
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-15 19:35:42 -0300

    reports generator tests

--- reports_generator/test/parser_test.exs
+++ reports_generator/test/parser_test.exs
@@ -6,26 +6,25 @@ defmodule ReportsGenerator.ParserTest do
     test "parse the file" do
       file_name = "report_test.csv"
 
-    response =
-      file_name
-      |> Parser.parse_file()
-      |> Enum.map(& &1)
+      response =
+        file_name
+        |> Parser.parse_file()
+        |> Enum.map(& &1)
 
-    expected_response = [
-      ["1", "pizza", 48],
-      ["2", "açaí", 45],
-      ["3", "hambúrguer", 31],
-      ["4", "esfirra", 42],
-      ["5", "hambúrguer", 49],
-      ["6", "esfirra", 18],
-      ["7", "pizza", 27],
-      ["8", "esfirra", 25],
-      ["9", "churrasco", 24],
-      ["10", "churrasco", 36]
-    ]
+      expected_response = [
+        ["1", "pizza", 48],
+        ["2", "açaí", 45],
+        ["3", "hambúrguer", 31],
+        ["4", "esfirra", 42],
+        ["5", "hambúrguer", 49],
+        ["6", "esfirra", 18],
+        ["7", "pizza", 27],
+        ["8", "esfirra", 25],
+        ["9", "churrasco", 24],
+        ["10", "churrasco", 36]
+      ]
 
-    assert response == expected_response
+      assert response == expected_response
     end
-
   end
 end
